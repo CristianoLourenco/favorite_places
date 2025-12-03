91b59efcc57cbf721c176c7e9c12fe94927cb1db
cristianoDev <cristianolourenco319@gmail.com>
2025-12-03 13:54:41 +0100

    storing the image locally

--- lib/providers/user_places_provider.dart
+++ lib/providers/user_places_provider.dart
@@ -9,6 +9,7 @@ import 'package:flutter/widgets.dart';
 import 'package:riverpod/legacy.dart';
 import 'package:path_provider/path_provider.dart' as syspaths;
 import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
 
 class UserPlacesProvider extends StateNotifier<List<PlaceModel>> {
   UserPlacesProvider() : super(const []);
@@ -17,7 +18,7 @@ class UserPlacesProvider extends StateNotifier<List<PlaceModel>> {
     final appDir = await syspaths.getApplicationDocumentsDirectory();
     final fileNmae = path.basename(image.path);
     final savedImage = await image.copy('${appDir.path}/$fileNmae');
-   
+
     debugPrint('Image saved at: ${savedImage.path}');
     log('Image saved at: ${savedImage.path}');
 
@@ -26,6 +27,30 @@ class UserPlacesProvider extends StateNotifier<List<PlaceModel>> {
       image: savedImage,
       location: location,
     );
+
+    final dbPath = await sql.getDatabasesPath();
+    final db = await sql.openDatabase(
+      path.join(dbPath, 'places.db'),
+      version: 1,
+      onCreate: (db, version) {
+        return db.execute('''CREATE TABLE user_places(
+            id TEXT PRIMARY KEY, 
+            title TEXT, 
+            image TEXT, 
+            lat REAL, 
+            lng REAL, 
+            address TEXT
+            )''');
+      },
+    );
+    await db.insert('user_places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+      'lat': newPlace.location.latitude,
+      'lng': newPlace.location.longitude,
+      'address': newPlace.location.address,
+    });
     state = [newPlace, ...state];
   }
 
